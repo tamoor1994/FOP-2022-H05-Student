4a753e31d31cacd5e0573e84d804837a076321d6
Alexander Staeding <alexanderstaeding@gmail.com>
2021-09-06 18:10:31 +0200

    Initial Commit

---
+++ src/test/java/H05/ExampleJUnitTest.java
@@ -0,0 +1,13 @@
+package H05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleJUnitTest {
+
+  @Test
+  public void testAddition() {
+    assertEquals(2, 1 + 1);
+  }
+}
