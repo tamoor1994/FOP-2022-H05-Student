5c3f57512dddd2c7b39cdaee3d5fca852bd8a637
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-11 15:22:47 +0100

    Add some javadoc

--- src/main/java/h05/Main.java
+++ src/main/java/h05/Main.java
@@ -1,6 +1,15 @@
 package h05;
 
+/**
+ * Main entry point in executing the program.
+ */
 public class Main {
+
+    /**
+     * Main entry point in executing the program.
+     *
+     * @param args program arguments, currently ignored
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- src/test/java/h05/ExampleJUnitTest.java
+++ src/test/java/h05/ExampleJUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * An example JUnit test class.
+ */
 public class ExampleJUnitTest {
 
     @Test
