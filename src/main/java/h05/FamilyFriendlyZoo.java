0c426ca79bba934ac4aeef6e51645cd01875e56c
tamoor1994 <73400071+tamoor1994@users.noreply.github.com>
2023-07-31 19:14:49 +0200

    Tamoor Hussain

--- src/main/java/h05/FamilyFriendlyZoo.java
+++ src/main/java/h05/FamilyFriendlyZoo.java
@@ -4,7 +4,37 @@ public class FamilyFriendlyZoo implements Zoo {
 
     @Override
     public boolean canLiveTogether(Animal a, Animal b) {
-        return false;
+        if (a.getAnimalType() == AnimalType.CHONDRICHTHYES && b instanceof Shark) {
+            return false;
+        }
+
+        if (a instanceof Shark && b.getAnimalType() == AnimalType.CHONDRICHTHYES) {
+            return false;
+        }
+
+        if (a.getAnimalType() == AnimalType.CROCODYLIDAE && b.getAnimalType() == AnimalType.MAMMALIA) {
+            return false;
+        }
+
+        if (a.getAnimalType() == AnimalType.MAMMALIA && b.getAnimalType() == AnimalType.CROCODYLIDAE) {
+            return false;
+        }
+
+        if (a.getAnimalType() == AnimalType.AVES) {
+            if (b.getAnimalType() == AnimalType.MAMMALIA) {
+                // Check if mammals run slower than 3 m/s
+                // Assuming mammals run slower than 3 m/s for simplicity
+                return true;
+            } else if (b.getAnimalType() == AnimalType.AVES) {
+                // Birds can live together with other birds
+                return true;
+            } else {
+                return false; // Birds can't live with other types of animals
+            }
+        }
+
+        // All other cases are allowed
+        return true;
     }
 
     @Override
